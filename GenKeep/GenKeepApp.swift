33f2e735d69c8fcb3622d88e0baa145dedd48ee8
Laura Belén Yachelini <lbyachelini@gmail.com>
2024-02-10 06:25:44 -0300

    Initial Commit

---
+++ GenKeep/GenKeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenKeepApp.swift
+//  GenKeep
+//
+//  Created by Laura Belen Yachelini on 10/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct GenKeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
